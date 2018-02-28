0dcb396add6ed5db5153eadcefc59e6d430c7007
ishak <ishak@dether.io>
2018-02-28 17:57:15 +0100

    [ADD] test map reducer

---
+++ src/reducers/map.test.js
@@ -0,0 +1,19 @@
+/* global beforeEach describe it expect */
+import deepFreeze from 'deep-freeze';
+
+import reducer from './map';
+
+describe('reducer::maps', () => {
+  let defaultAction;
+
+  beforeEach(() => {
+    defaultAction = {
+      userPosition: { lat: 48.8628, lng: 2.3292 },
+    };
+    deepFreeze(defaultAction);
+  });
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(defaultAction);
+  });
+});
