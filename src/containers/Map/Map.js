a31775a657e20ae89d31a7f129e8093048edd6e3
Gael BOYENVAL <gaelboyenval@icloud.com>
2018-03-03 12:27:04 +0100

    fix didix coding style

--- src/containers/Map/Map.js
+++ src/containers/Map/Map.js
@@ -20,8 +20,8 @@ import { distance, getClusterData } from '../../helpers/map';
 
 const MapWrapper = styled.div`
   position: relative;
-  width: 50vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
 
   /* hide google legal mentions */
