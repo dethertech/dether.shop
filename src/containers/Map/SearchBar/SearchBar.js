70dafb38a8f906b64e8a5eb794e083ae0fc9acd9
Didix <edxcollot@gmail.com>
2018-03-03 09:15:08 +0100

    [ADD] SearchBar for Map

---
+++ src/containers/Map/SearchBar/SearchBar.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import {
+  setCenterPosition as setCenterPositionAction,
+  fetchAll as fetchAllAction
+} from '../../../actions/map';
+import SearchBarWrapper from './SearchBarWrapper';
+
+export class SearchBar extends PureComponent {
+  static propTypes = {
+    setCenterPosition: PropTypes.func.isRequired,
+    fetchAll: PropTypes.func.isRequired
+  };
+
+  state = {
+    address: ''
+  };
+
+  onChange = address => this.setState({ address });
+
+  getAddress = async () => {
+    const { setCenterPosition, fetchAll } = this.props;
+    const place = (await geocodeByAddress(this.state.address))[0];
+    const position = await getLatLng(place);
+    setCenterPosition(position);
+    fetchAll(position);
+  };
+
+  render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange
+    };
+    return (
+      <SearchBarWrapper>
+        <PlacesAutocomplete onSelect={this.getAddress} inputProps={inputProps} />
+      </SearchBarWrapper>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  setCenterPosition: bindActionCreators(setCenterPositionAction, dispatch),
+  fetchAll: bindActionCreators(fetchAllAction, dispatch)
+});
+export default connect(null, mapDispatchToProps)(SearchBar);
