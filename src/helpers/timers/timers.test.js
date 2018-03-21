f65cd482ae2d58a12c71a2ba41ccd4e4b9b36478
ishak <ishak@dether.io>
2018-03-21 17:35:45 +0100

    [UPD] timer test

--- src/helpers/timers/timers.test.js
+++ src/helpers/timers/timers.test.js
@@ -1,6 +1,6 @@
 /* global describe it expect */
 
-import { timeSince, phoneVerificationTime } from './timers';
+import { phoneVerificationTime } from './timers';
 
 describe('helpers::timers', () => {
   let time;
@@ -9,13 +9,9 @@ describe('helpers::timers', () => {
     time = new Date();
   })
 
-  it('should get time since a phone verification', () => {
-    expect(timeSince(time)).toEqual(0)
-  })
-
   it('should check if the phone verification time is valid', () => {
     expect(phoneVerificationTime(time)).toEqual(true)
-    expect(phoneVerificationTime(time - 14999)).toEqual(true)
+    expect(phoneVerificationTime(time - 14000)).toEqual(true)
     expect(phoneVerificationTime(time - 15000)).toEqual(false)
     expect(phoneVerificationTime(time - 150000)).toEqual(false)
   })
