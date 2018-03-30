48c6e7624f0b83e5fad68df17f93647ee24213bd
Didix <edxcollot@gmail.com>
2018-03-30 17:53:06 +0200

    [ADD] tests to containers

---
+++ src/containers/AddShop/Form/Form.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { Form } from './Form';
+
+describe('Containers::AddShop::Form', () => {
+  let wrapper;
+  const shop = {
+    lat: null,
+    lng: null,
+    address: '40 rue Citadelle Paris',
+    countryId: 'fr',
+    postalCode: '75010',
+    name: 'Magasin',
+    description: 'Super Magasin',
+    cat: 'cool',
+    opening: '0000000'
+  };
+
+  const props = {
+    shop,
+    setDataShopPending: () => null,
+    onSubmit: () => null
+  }
+  const component = props => <Form {...props} />
+
+  it('should match shallow snapshop', () => {
+    expect(shallow(component(props))).toMatchSnapshot('Form::shallow');
+  });
+  it('should match mounted snapshop', () => {
+    expect(toJson(component(props))).toMatchSnapshot('Form::mount');
+  });
+});
