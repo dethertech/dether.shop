40535269fbeedbe471abbddfbe1847305b236248
Didix <edxcollot@gmail.com>
2018-04-06 10:48:06 +0200

    [UPD] doc and env-example

--- src/actions/user/user.js
+++ src/actions/user/user.js
@@ -1,17 +1,10 @@
 import { config } from '../../constants';
-/**
- * setEthAddress
- * @param {[type]} ethAddress [description]
- */
+
 const setEthAddress = ethAddress => ({
   type: 'SET_ETH_ADDRESS',
   payload: ethAddress,
 });
 
-/**
- * setBalance
- * @param {[type]} balance [description]
- */
 const setBalance = balance => ({
   type: 'SET_BALANCE',
   payload: balance,
