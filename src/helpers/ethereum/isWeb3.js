90123f7c1ae049a6b756d8f505b17cc9516d6884
Eamon-McNamee <eamonmcnamee@gmail.com>
2018-10-29 19:49:43 +0700

    [UPD] explicitly return only after await for account enable

--- src/helpers/ethereum/isWeb3.js
+++ src/helpers/ethereum/isWeb3.js
@@ -7,12 +7,14 @@ import { web3js } from './utils';
 const isWeb3 = async () => {
   const isModernWeb3 = typeof window.ethereum !== 'undefined';
   const isLegacyWeb3 = typeof window.web3 !== 'undefined';
-  if (isModernWeb3 || isLegacyWeb3) {
-    if (isModernWeb3) {
-      await window.ethereum.enable();
-    }
-    const address = (await web3js.eth.getAccounts())[0];
-    return address;
+  let addresses;
+  if (isModernWeb3) {
+    await window.ethereum.enable();
+    addresses = await web3js.eth.getAccounts();
+    return addresses[0];
+  } else if (isLegacyWeb3) {
+    addresses = await web3js.eth.getAccounts();
+    return addresses[0];
   }
   return null;
 };
